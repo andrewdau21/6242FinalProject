cb6a4b3408e38134516974c185badd6d42a39a6e
Reed Kottke <reed>
2021-10-26 20:41:53 -0500

    initial backend code

---
+++ backend/application.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import file_helper as FileHelper
+
+#Source for application
+
+#Bring in the names
+player_jon_lester = FileHelper.load_players(("Jon", "Lester"))
+
+#Bring in the at bats for a pitcher
+jon_lester_atbats = FileHelper.load_atbats(player_jon_lester.loc[0].at['id'])
+
+#Bring in the pitches for the at basts for a pitcher
+jon_lester_pitches = FileHelper.load_pitches(jon_lester_atbats['ab_id'].tolist())
