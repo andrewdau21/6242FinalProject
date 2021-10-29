b2b7de6a91cd74f30f95125bf5f79371b714e211
Reed Kottke <reed>
2021-10-28 20:00:35 -0500

    adding pitch class and previous pitch class

--- backend/application.py
+++ backend/application.py
@@ -11,3 +11,16 @@ jon_lester_atbats = FileHelper.load_atbats(player_jon_lester.loc[0].at['id'])
 
 #Bring in the pitches for the at basts for a pitcher
 jon_lester_pitches = FileHelper.load_pitches(jon_lester_atbats['ab_id'].tolist())
+
+#Split into train and test
+
+
+#Train model(s)
+
+#Test model(s)
+
+#Compare model(s)
+
+#Build real model
+
+#Build classification output [real output dataset]
--- backend/file_helper.py
+++ backend/file_helper.py
@@ -1,6 +1,10 @@
-from numpy import NaN, nan
+from numpy import nan
 import pandas as pd
-valid_pitches = ['CH', 'CU', 'EP', 'KC', 'KN', 'SC', 'SL','FA', 'FC', 'FF', 'FS', 'FT', 'SI']
+valid_pitches = ['CH', 'CU', 'KC', 'KN', 'SC', 'SL','FA', 'FC', 'FF', 'FS', 'FT', 'SI']
+fastballs = ['FC', 'FF', 'FS', 'FT', 'SI','FA']
+offspeeds = ['CH', 'KN']
+breakings = ['CU', 'KC', 'SC', 'SL']
+columns_of_concern = ['ab_id', 'pitch_num', 'b_count', 's_count', 'on_1b', 'on_2b', 'on_3b', 'outs', 'pitch_class', 'prev_pitch_class']
 
 def load_players(name=('', '')):
     players = pd.read_csv('./data/player_names.csv')
@@ -18,6 +22,28 @@ def load_atbats(pitcher_id = ''):
     atbats['stand'] = atbats['stand'].replace('R', 0.0)
     return atbats
 
+def pitch_class(pitch_type):
+    if (pitch_type in fastballs):
+        return 'fast'
+    elif (pitch_type in offspeeds):
+        return 'off'
+    elif (pitch_type in breakings):
+        return 'break'
+    else:
+        print('Found a pitch I do not recognize: {}'.format(pitch_type))
+        return 'unknown'
+
+def append_previous_pitch_class(pitches):
+    temp_pitches = pitches.copy().reset_index()
+    temp_pitches['key'] = temp_pitches.apply(lambda x: int('{}{}'.format(int(x['ab_id']), int(x['pitch_num']))), axis=1)
+    temp_pitches['prev_key'] = temp_pitches['key'].apply(lambda x: x-1)
+    joined_temp_pitches = temp_pitches.merge(right=temp_pitches,left_on='prev_key', right_on='key', how='left')
+    joined_temp_pitches = joined_temp_pitches[['index_x','pitch_class_y']]
+    pitches = pitches.merge(right=joined_temp_pitches, left_index=True, right_on='index_x')
+    pitches = pitches.rename(columns={'pitch_class_y':'prev_pitch_class'})
+    pitches['prev_pitch_class'] = pitches['prev_pitch_class'].replace(nan, 'unknown')
+    return pitches
+
 def load_pitches(atbat_ids = ''):
     pitches = pd.read_csv('./data/pitches.csv')
     pitches = pitches.reindex(sorted(pitches), axis=1)
@@ -26,5 +52,7 @@ def load_pitches(atbat_ids = ''):
 
     # remove anything that's not a recognized pitch type
     pitches = pitches[pitches['pitch_type'].isin(valid_pitches)]
-    
-    return pitches
+    pitches['pitch_class'] = pitches['pitch_type'].apply(lambda x: pitch_class(x))
+    pitches = append_previous_pitch_class(pitches)
+
+    return pitches[columns_of_concern]
