c8376d13cf6f6b488808bf699bbe8cbd0fb66786
andrewdau21 <44346254+andrewdau21@users.noreply.github.com>
2021-11-20 22:23:59 -0500

    Update app.R
    changes to UI layouts.  updated to sidebar view.  Cleaned up the display on model run.

--- app.R
+++ app.R
@@ -19,6 +19,7 @@ library(tidyr)
 library(htmltools)
 library(reactable)
 library(shinybusy)
+library(shinyjs)
 
 
 # Define UI for application that draws a histogram
@@ -31,7 +32,8 @@ ui <- navbarPage("Next Pitch",
     tabPanel("Simulator",
         # Show a plot of the generated distribution
         tabPanel("simulator",
-          column(width=4,
+         mainPanel(
+          column(width=8,
           selectizeInput('pitcher_select', 'Select a Pitcher:', choices = c("Jon Lester"), selected = NULL, multiple = FALSE,
                          options = NULL),
           selectizeInput('previous_pitch', 'Select a Previous Pitch:', choices = c("None" = 0, "Fastball"=1, "Offspeed" = 2, "Breaking" = 3), selected = NULL, multiple = FALSE,
@@ -42,10 +44,18 @@ ui <- navbarPage("Next Pitch",
            plotly::plotlyOutput("diamond_plotly")
           )
           ,
-          column(width = 3,
+          column(
+            
+            
+            width = 4,
+            
                  radioButtons("balls", label = h3("Balls"),
                               choices = list("0" = 0, "1" = 1, "2" = 2, "3" = 3), 
                               selected = NULL),
+            
+                 
+                 
+                 
           radioButtons("strikes", label = h3("Strikes"),
                        choices = list("0" = 0, "1" = 1, "2" = 2), 
                        selected = NULL),
@@ -55,21 +65,38 @@ ui <- navbarPage("Next Pitch",
           actionButton("runit",label="Run Algorithm",  icon("running"), 
                        style="color: #fff; background-color: #337ab7; border-color: #2e6da4")
           )
+         )
           
           ,
-          column(width = 5,
-                h3("Reserved Space for Model Output"),
+         sidebarPanel(
+          #column(width = 5,
+                #h3("Reserved Space for Model Output"),
                 #dataTableOutput("outputtable"),
+                uiOutput("initial_message"),
+                uiOutput("initial_message2"),
+                uiOutput("pitchprobtitle"),
                 reactableOutput("pitchprob"),
-                h3("This is just an example heat map"),
-                h4("This is getting busy, need to clean"),
+                br(),
+                br(),
                 plotlyOutput("example_heat"),
-                h3("Current Parameters"),
-                verbatimTextOutput("parms"),
-                verbatimTextOutput("parms2"),
-                verbatimTextOutput("parms3"),
-                verbatimTextOutput("parms4")
-                )
+                br(),
+                br(),
+                br(),
+                uiOutput("modelparms"),
+                uiOutput("modelparmsballs"),
+                uiOutput("modelparmsstrikes"),
+                uiOutput("modelparmsouts"),
+                uiOutput("modelparmsprev")
+                #h3("This is just an example heat map"),
+                #h4("This is getting busy, need to clean"),
+                
+                #h3("Current Parameters"),
+                
+                #verbatimTextOutput("parms2"),
+                #verbatimTextOutput("parms3"),
+                #verbatimTextOutput("parms4")
+               # )
+         )
         ),
         
     ),
@@ -93,16 +120,21 @@ server <- function(input, output) {
   #colorvec <- reactive({
   #  c("black","black","black","black","black")
   #})
+  
+  #observe({
+   # hide(id = "initial_message", condition = input$runit)
+  #})
     
   observeEvent(input$runit,{
     
     #show_modal_spinner()
                print("you clicked runnit")
     
-    
-    
-  
-      
+    #shinyjs:: hide(id = "#initial_message.shiny-html-output.shiny-bound-output")
+    output$initial_message <- renderUI(h3(""))
+    output$initial_message2 <- renderUI(h3(""))
+    output$pitchprobtitle <- renderUI(h4("Probability of Next Pitch:"))
+   
       db_name <- "stats_main"
       db_user <- "admin"
       db_password <- "cSe6242!"
@@ -114,6 +146,7 @@ server <- function(input, output) {
       outs <- isolate(input$outs)
       previouspitch = isolate(input$previous_pitch)
       
+     
     
       
       if (colorvec$colors[2] == "red")
@@ -133,6 +166,12 @@ server <- function(input, output) {
       }
       else{runneronthird = 0}
       
+      
+      output$modelparms <- renderUI("The model was executed with the following parameters:")
+      output$modelparmsballs <- renderUI(paste0("Balls: ", balls))
+      output$modelparmsstrikes <- renderUI(paste0("Strikes: ", strikes)) 
+      output$modelparmsouts <- renderUI(paste0("Outs: ", outs))
+      output$modelparmsprev <- renderUI(paste0("Previous Pitch: ", previouspitch))
       
       
       print('runner first')
@@ -175,23 +214,62 @@ server <- function(input, output) {
         div(style = list(display = "flex", alignItems = "center"), label, chart)
       }
       
-      
+      #print(df.long)
+      df.long$Pitch <- gsub("(?<=\\b)([a-z])", "\\U\\1", tolower(df.long$Pitch), perl=TRUE)
+      df.long$Probability <- round(df.long$Probability,3)
       
       #output$outputtable <- DT::renderDataTable({
       #  DT::datatable(df.long)
         
       output$pitchprob <- renderReactable({
         
+        
+        
         reactable(df.long,
+                  defaultColDef = colDef(
+                    header = function(value) gsub(".", " ", value, fixed = TRUE),
+                    cell = function(value) format(value, nsmall = 1),
+                    align = "center",
+                    minWidth = 70,
+                    headerStyle = list(background = "#a8a8ad")
+                  ),
                   columns = list(
                     Probability = colDef(name = "Probability", align = "left", cell = function(value) {
-                      width <- paste0(value /.01, "%")
+                      width <- paste0(value/.01 , "%")
                       bar_chart(value, width = width, background = "#e1e1e1")
                     })
-                  )
+                  ),
+                  bordered = TRUE,
+                  highlight = TRUE
         )
         
         })
+      
+      output$example_heat <- renderPlotly({
+        strike_zones <- data.frame(
+          x1 = rep(-1.5:0.5, each = 3),
+          x2 = rep(-0.5:1.5, each = 3),
+          y1 = rep(1.5:3.5, 3),
+          y2 = rep(2.5:4.5, 3),
+          z = factor(c(7, 4, 1, 8, 5, 2, 9, 6, 3))
+        )
+        
+        strike_zones$labcol <- c("red","red","yellow","orange","orange","red","yellow","red","red")
+        
+        abc <- ggplot() +
+          xlim(-3, 3) + xlab("") +
+          ylim(0, 6) + ylab("") +
+          geom_rect(data = strike_zones,
+                    aes(xmin = x1, xmax = x2, ymin = y2, ymax = y1), fill = strike_zones$labcol, color = "grey20") +
+          geom_text(data = strike_zones,
+                    aes(x = x1 + (x2 - x1)/2, y = y1 + (y2 - y1)/2, label = z),
+                    size = 7, fontface = 2, color = I("grey20")) +
+          theme_bw() + theme(legend.position = "none")
+        
+        
+        ggplotly(abc)
+      }
+      )
       
    # })
     
@@ -208,15 +286,16 @@ server <- function(input, output) {
     output$diamond_plotly <- renderPlotly({
       
       bases =data.frame(x=c(0, 30, 0, -30, 0),
-                        y=c(0,75, 150, 75, 0)
+                        y=c(0,75, 150, 75, 0),
+                        z =c("Home", "First Base", "Second Base", "Third Base", "Home")
       )
       #print (colorvec())
       #colorsel <- c("black","black","red","red","black")
       bases$colorsel <- colorvec$colors
 
-      
+      print(bases)
     
-      fig <- plot_ly(data = bases, x = ~x, y = ~y, source = "diamond_c",
+      fig <- plot_ly(data = bases, x = ~x, y = ~y, text=~z, hoverinfo = 'text', source = "diamond_c",
                      
                      marker = list(size = 14,
                                    symbol = 'square',
@@ -256,31 +335,7 @@ server <- function(input, output) {
       
     })
     
-    output$example_heat <- renderPlotly({
-      strike_zones <- data.frame(
-        x1 = rep(-1.5:0.5, each = 3),
-        x2 = rep(-0.5:1.5, each = 3),
-        y1 = rep(1.5:3.5, 3),
-        y2 = rep(2.5:4.5, 3),
-        z = factor(c(7, 4, 1, 8, 5, 2, 9, 6, 3))
-      )
-      
-      strike_zones$labcol <- c("red","red","yellow","orange","orange","red","yellow","red","red")
-      
-      abc <- ggplot() +
-        xlim(-3, 3) + xlab("") +
-        ylim(0, 6) + ylab("") +
-        geom_rect(data = strike_zones,
-                  aes(xmin = x1, xmax = x2, ymin = y2, ymax = y1), fill = strike_zones$labcol, color = "grey20") +
-        geom_text(data = strike_zones,
-                  aes(x = x1 + (x2 - x1)/2, y = y1 + (y2 - y1)/2, label = z),
-                  size = 7, fontface = 2, color = I("grey20")) +
-        theme_bw() + theme(legend.position = "none")
-      
-      
-      ggplotly(abc)
-    }
-    )
+   
     
     
     observeEvent(event_data("plotly_click", source = "diamond_c", priority = "event"),{
@@ -367,7 +422,10 @@ server <- function(input, output) {
       
       })
     
- 
+    
+    output$initial_message <- renderUI(h3("Select Parameters"))
+    output$initial_message2 <- renderUI(h3("Click Run Algorithm"))
+   
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
